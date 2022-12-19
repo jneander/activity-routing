5ba5661b2d73d30c5df2fd99ab0922e5ea8d16ac
Jeremy Neander <jeremy.neander@gmail.com>
2022-12-19 09:08:32 -0600

    fix: remove checked-in dist

--- dist/cjs/index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Router = void 0;
-var router_1 = require("./router");
-Object.defineProperty(exports, "Router", { enumerable: true, get: function () { return router_1.Router; } });
